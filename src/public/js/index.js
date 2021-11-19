a67a5b322a4089e9b97c4fd9633264be239acc2e
Farruh Olimov <farruhbekolimov3904@gmail.com>
2021-11-20 03:01:46 +0500

    add sidenav for mobile

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,8 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, {
+
+document.addEventListener('DOMContentLoaded', function () {
+    var selects = document.querySelectorAll('select');
+    var selectInstances = M.FormSelect.init(selects, {
         classes: "select"
     });
-  });
 
-console.log("TEST")
+    var sidenavs = document.querySelectorAll('.sidenav');
+    var sidenavInstances = M.Sidenav.init(sidenavs, {
+        edge: "right",
+        draggable: true,
+        preventScrolling: true,
+        onOpenEnd: disableBodyScroll(),
+        onCloseEnd: enableBodyScroll()
+    });
+});
+
+function disableBodyScroll(){
+    document.body.style.overflow = "hidden"
+}
+function enableBodyScroll(){
+    document.body.style.overflow = "auto"
+}
+
+var dropdowns = document.querySelectorAll('.dropdown-trigger')
+for (var i = 0; i < dropdowns.length; i++) {
+    M.Dropdown.init(dropdowns[i]);
+}
