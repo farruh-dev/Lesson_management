7d9c807b8c832ca28e2366f49fa5ed9e5e17845c
Farruh Olimov <farruhbekolimov3904@gmail.com>
2022-03-21 02:38:00 +0500

    remove levels

--- src/modules/mongoose.js
+++ src/modules/mongoose.js
@@ -1,21 +1,18 @@
 const mongoose = require('mongoose');
 const init = require('./init');
-const admins = require('../models/AdminModel');
 require("dotenv").config()
 
+const admins = require('../models/AdminModel');
 require("../models/UsersModel")
 require("../models/AdminModel")
 require("../models/ScheduleModel")
 require("../models/LessonModel")
-require("../models/LevelsModel")
 require("../models/GroupModel")
 require("../models/VideosModel")
 
 async function mongo(){
     try {
         await mongoose.connect(process.env.MONGODB_URL)
-        
-        await init(admins)
 
     } catch (error) {
         console.log("MONGO_ERROR: ", error);
